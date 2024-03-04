e48501a11d97fcd560b806d24a6cb986887caa9f
M00NLI6H7 <M00NLI6H7@protonmail.com>
2024-03-04 21:06:50 +0100

    leetcode acceptable asm solution

--- 11-Container-With-Most-Water/solution.s
+++ 11-Container-With-Most-Water/solution.s
@@ -2,46 +2,55 @@
 
 .global _maxArea
 
-_maxArea:                                  // int maxArea(int* heights, int heightsSize)
-    push rbp                               // {
-    mov rbp, rsp                           //
+_maxArea:
+    push rbp
+    mov rbp, rsp
 
-    mov r8, -2147483648                    //   int maxArea = INT_MIN;
-    mov r9, 0                              //   int l = 0;
-    mov r10, rsi                           //   int r;
-    sub r10, 1                             //   r = heightsSize - 1;
+    sub rsp, 8
+    mov qword ptr [rsp + 0], rdi
 
-.WHILE_LOOP:                               //   while (
-    cmp r10, r9                            //          r > l
-    jle .WHILE_LOOP_EXIT                   //   )
-                                           //   {
-    mov r11, r10                           //     int length = r;
-    sub r11, r9                            //     length -= l;
-    mov r12d, dword ptr [rdi + r9 * 4]     //     int lheight = heights[l];
-    mov r13d, dword ptr [rdi + r10 * 4]    //     int rheight = heights[r];
-                                           //     int hight = lheight;
-    cmp r12d, r13d                         //     if (heights[l] >= heights[r]) {
-    jle .L_LESS_THAN_R                     //
-                                           //
-    mov r12d, r13d                         //       hight = rheight;
-    sub r10, 1                             //       r--;
-    jmp .CALCULATE_AREA                    //     }
+    mov r8, -2147483648
+    mov r9, 0
+    mov r10, rsi
+    sub r10, 1
 
-.L_LESS_THAN_R:                            //     else {
-    add r9, 1                              //       l++;
-    jmp .CALCULATE_AREA                    //     }
+.WHILE_LOOP:
+    cmp r10, r9
+    jle .WHILE_LOOP_EXIT
+
+    mov r11, r10
+    sub r11, r9
+    
+    mov rax, qword ptr [rsp + 0]
+    mov eax, dword ptr [rax + r9 * 4]
+    mov esi, eax
+
+    mov rax, qword ptr [rsp + 0]
+    mov eax, dword ptr [rax + r10 * 4]
+    mov edi, eax
+
+    cmp esi, edi
+    jle .L_LESS_THAN_R
+    mov esi, edi
+    sub r10, 1
+    jmp .CALCULATE_AREA
+
+.L_LESS_THAN_R:
+    add r9, 1
+    jmp .CALCULATE_AREA
 
 .CALCULATE_AREA:
-    imul r11, r12                          //     int area = hight * length
-    cmp r11, r8                            //     if (area > maxArea) {
-    jg .SET_NEW_MAX_AREA                   //
-    jmp .WHILE_LOOP                        //
+    imul rsi, r11
+    cmp rsi, r8
+    jg .SET_NEW_MAX_AREA
+    jmp .WHILE_LOOP
 
 .SET_NEW_MAX_AREA:
-    mov r8, r11                            //       maxArea = area;
-    jmp .WHILE_LOOP                        //     }
+    mov r8, rsi
+    jmp .WHILE_LOOP
 
-.WHILE_LOOP_EXIT:                          //   } // while
-    mov eax, r8d                           //   int returnValue = maxArea;
-    pop rbp                                //   return returnValue;
-    ret                                    // }
+.WHILE_LOOP_EXIT:
+    mov eax, r8d
+    add rsp, 8
+    pop rbp
+    ret
